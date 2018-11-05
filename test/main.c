0d692671758ce6e8f6f97a76202972ed347a816a
lxp <lxp@ufee.local>
2018-11-05 11:04:12 +0800

    Initial Commit

---
+++ test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by lxp on 2018/11/5.
+//  Copyright © 2018年 lxp. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
